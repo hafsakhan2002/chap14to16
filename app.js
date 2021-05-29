9204fdc0709c5d8dfe1abc3764cf96b07881f84d
hafsakhan2002 <hafsa.khan14.2002@gmail.com>
2021-05-29 20:56:07 +0500

    ch14to16

---
+++ app.js
@@ -0,0 +1,145 @@
+// ..................................ARRAYS..(CHAPTERS 13-15).........................................
+
+
+
+
+
+// var s1= [];
+// var s2= [];
+// var s2= [{" ": 0, " " : 0}];
+
+// var stringarray = ["hafsa", "amin", "Khan"];
+// var numbersarray= [1,2,3,4,5];
+// var booleanarray1= 10>9;
+// @@ -582,28 +624,227 @@
+// var qua = [ "SSC", "HSC", "BCS","BS", "BCOM", "MS", "M Phil", "PhD"]
+// document.write("<h1> Qualifications</h1>" + qua[0] + "</br>" + qua[1] + "</br>" + qua[2] + "</br>"+ qua[3] + "</br>" + qua[4] + "</br>" + qua[5] + "</br>" + qua[6] + "</br>" + qua[7] + "</br>");
+
+
+// var stu = ["hafsa", "hania", "hiba"];
+// var marks= [450,420,440];
+// var totalmarks = 500;
+// document.write("Score of"+" " +stu[0]+ " "+"is"+ " " +marks[0]+ " "+" ."+"percentage"+ " "+marks[0]/totalmarks*100+"%"+ "<br>");
+// document.write("Score of"+" " +stu[1]+ " "+"is"+ " " +marks[1]+ " "+" ."+"percentage"+ " "+marks[1]/totalmarks*100+"%" + "<br>");
+// document.write("Score of"+" " +stu[2]+ " "+"is"+ " " +marks[2]+ " "+" ."+"percentage"+ " "+marks[2]/totalmarks*100+"%");
+
+// var color = ["Red", "green", "blue", "yellow","orange"];
+// document.write(color+"<br>");
+// color.unshift(prompt("Enter any color you want to add"));
+// document.write(color+"<br>");
+// color.push(prompt("Enter any color you want to add at the end"))
+// document.write(color+"<br>");
+// color.unshift("Lime","Aqua");
+// document.write(color+ "<br>");
+// color.shift ();
+// document.write(color+ "<br>");
+// color.pop();
+// document.write(color+ "<br>");
+// color.pop();
+// document.write(color+ "<br>");
+
+
+// var colour= ["Blue", "Green", "Red"];
+// console.log(colour);
+// document.write(colour + "<br>")
+// colour.unshift(prompt("Enter any colour"));
+// console.log(colour)
+// document.write(colour + "<br>")
+// colour.push(prompt("Enter any colour to add it at the end"));
+// console.log(colour);
+// document.write(colour + "<br>")
+// colour.unshift("Black","White");
+// console.log(colour);
+// document.write(colour + "<br>")
+// colour.pop()
+// console.log(colour);
+// document.write(colour + "<br>")
+
+// colour.splice(prompt("where you want to add colour 0-3"),0,(prompt("enter what colour you want to add")))
+// console.log(colour);
+// document.write(colour + "<br>")
+
+// colour.splice(prompt("from which index you want to delete colour enter 0-2"),(prompt("Enter how many you want to delete")));
+// console.log(colour)
+// document.write(colour + "<br>")
+
+
+
+// var students = [320,350,202,109,450];
+// console.log(students);
+// var studentssorted = students.sort();
+// console.log(studentssorted)
+
+
+// var cities = ["Karachi","Hyderabad","Lahore","Islamabad","Quetta"];
+// console.log(cities);
+// var selectedcities = cities.slice(3,5);
+// console.log(selectedcities);
+
+
+
+// var arr = ["This" ,  "is" ,  "my" ,  "cat"];
+// console.log(arr);
+// var arrjoin = arr.join(" ");
+// console.log(arrjoin)
+
+
+
+// var  arr = ["Keyboard", "Mouse", "Printer", "Monitor"]
+// console.log(arr);
+// document.write(arr + "<br>")
+
+
+// var firstout= arr.shift()
+// console.log(firstout)
+// document.write(firstout+ "<br>")
+// var firstout= arr.shift()
+// console.log(firstout)
+// document.write(firstout+ "<br>")
+// var firstout= arr.shift()
+// console.log(firstout)
+// document.write(firstout+ "<br>")
+// var firstout= arr.shift()
+// console.log(firstout)
+// document.write(firstout+ "<br>")
+
+// arr.unshift("Table");
+// console.log(arr)
+// arr.unshift("Mobile");
+// console.log(arr)
+// arr.unshift("CPU");
+// console.log(arr);
+// arr.unshift("Laptop");
+// console.log(arr);
+// arr.unshift("Bat");
+// console.log(arr);
+
+
+
+// arr.push("Table");
+// console.log(arr);
+// arr.push("Mobile");
+// console.log(arr);
+// arr.push("CPU");
+// console.log(arr);
+// arr.push("Laptop");
+// console.log(arr);
+// arr.push("Bat");
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr)
+// arr.pop();
+// console.log(arr)
+// arr.pop();
+// console.log(arr)
+// arr.pop();
+// console.log(arr)
+
+
+// var arr = ["Apple", "Samsung", "Motorola", "Nokia", "Sony" , "Haier"];
+// console.log(arr);
+// document.write("<select>" + "<option>" + arr[0] + "</option>" +   "<option>" + arr[1] + "</option>"  + "<option>" + arr[2] + "</option>"  + "<option>" + arr[3] + "</option>"  + "<option>" + arr[4] + "</option>"  + "<option>" + arr[6] + "</option>" + "</select>");
+// .......................................ALL ASSIGNMENTS COMPLETED..................................... 
